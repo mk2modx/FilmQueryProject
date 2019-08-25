23766e8bad3715bc040b61e325fd458e7efc8504
Mark Ag <mk2modx@icloud.com>
2019-08-24 18:24:08 -0600

    MVP

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -36,44 +36,65 @@ public class FilmQueryApp {
 	  int selection = 0;
 	  while(cont) {
     Scanner kb = new Scanner(System.in);
-	System.out.println("Welcome to Blockbuster!");
-    System.out.println();
-    System.out.println();
-    System.out.println("Select an option");
-    System.out.println();
-    System.out.println("1-Look up film by id");
-    System.out.println("2-Look up film by a search keyword");
-    System.out.println("3-Exit");
+    System.out.println("|-----------------------------------------------|");
+	System.out.println("|        Welcome to VIDEO DEPOT                 |");
+	System.out.println("|                                               |");
+	System.out.println("|                                               |");
+    System.out.println("|                                               |");
+    System.out.println("|           Select an option                    |");
+    System.out.println("|                                               |");
+    System.out.println("|       1-Look up film by id                    |");
+    System.out.println("|       2-Look up film by a search keyword      |");
+    System.out.println("|       3-Exit                                  |");
+    System.out.println("|-----------------------------------------------|");
 	 
     selection = kb.nextInt();
-    switch (selection) {
-    case 1:
-    	System.out.println("Enter film id 1-1000");
-    	int filmId = kb.nextInt();
-    	Film film = db.findFilmById(filmId);
-    	film.filmDetails();
-    	List<Actor> actors = db.findActorsByFilmId(filmId);
-    	System.out.println("\n\nFeatured Cast : ");
-    	for (Actor actor : actors) {
-			actor.printCast();
+    try {
+		switch (selection) {
+		case 1:
+			System.out.println("Enter film id 1-1000");
+			int filmId = kb.nextInt();
+			Film film = db.findFilmById(filmId);
+			film.filmDetails();
+//			List<Actor> actors = db.findActorsByFilmId(filmId);
+//			System.out.println("\n\nFeatured Cast : ");
+//			for (Actor actor : actors) {
+//				actor.printCast();
+//			}
+			break;
+		case 2:
+			System.out.println("Enter keyword");
+			String keyWord = kb.next();
+			List<Film> films = db.searchByKeyword(keyWord);
+		for (Film film2 : films) {
+		    film2.filmDetails();
+			
 		}
-    	break;
-    case 2:
-    	System.out.println("Enter keyword");
-    	String keyWord = kb.next();
-    	List<Film> films = db.searchByKeyword(keyWord);
-    for (Film film2 : films) {
-        film2.filmDetails();
-		
+		if (films.size() == 0) {
+			System.out.println();
+			System.out.println();
+			System.out.println("----------NO RESULT FOUND-------------");
+			System.out.println();
+			System.out.println();
+		}
+			break;
+		case 3:
+			System.out.println("Goodbye");
+			cont = false;
+			break;
+		default:
+			System.out.println();
+			System.out.println("-------------Invalid Input----------------");
+			System.out.println("-------------NO SUCH FILM----------------");
+			System.out.println();
+			break;
+		}
+	} catch (Exception e) {
+		System.out.println();
+		System.out.println(" ---------------Invalid Input------------------- ");
+		System.out.println(" ---------------NO SUCH FILM------------------- ");
+		System.out.println();
 	}
-    	break;
-    case 3:
-    	System.out.println("Goodbye");
-    	cont = false;
-    	break;
-    default:
-    	break;
-    }
 	  
   }
   
